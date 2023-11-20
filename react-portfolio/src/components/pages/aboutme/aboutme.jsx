bc696b708b424d046db45d0f1c1e32122f69b65a
joseph <jomill0723@outlook.com>
2023-11-20 17:06:23 -0500

    added to ab me css and jsx

---
+++ react-portfolio/src/components/pages/aboutme/aboutme.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card, Row, Col, Container, Tabs, Tab } from 'react-bootstrap';
+import './aboutme.css';
+import '../../../style.css';
+import mypic from '../../../assets/images/mypic.jpg';
+
+
+function aboutMe() {
+    return (
+        <Container fluid>
+            <Row className="justify-content-center overflow-x-hidden my-3">
+                <Col className="image-container order-2 order-md-1 d-flex justify-content-center my-2" lg={2} md={4} sm={12}>
+                    <img src={mypic} alt="Profile" className="profile-pic drop-image" />
+                </Col>
+                <Col className='order-1 order-md-2' lg={7} md={7} sm={12}>
+                    <Tabs defaultActiveKey="ab-me" id="">
+
+                        <Tab eventKey="ab-me" title="ab me">
+                            <p className='about-me-text'>Hi, my name is Joseph. </p>
+                        </Tab>
+
+                    </Tabs>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default aboutMe;
