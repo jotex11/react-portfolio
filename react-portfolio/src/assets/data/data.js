99e9d2c0df14e1bad4e606968d32a74d1c1550f9
joseph <jomill0723@outlook.com>
2023-11-20 16:42:23 -0500

    added data file, and updated project.jsx

---
+++ react-portfolio/src/assets/data/data.js
@@ -0,0 +1,35 @@
+import portfolio from 'react-portfolio/src/assets/images/projects/portfolio.png';
+import quiz from 'react-portfolio/src/assets/images/projects/quiz.jpeg';
+import tunecast from 'react-portfolio/src/assets/images/projects/tunecast.jpeg';
+import milbook from 'react-portfolio/src/assets/images/projects/milbook.png';
+
+const projectInfo = [
+    {
+    name: 'Portfolio',
+    description: 'A front end portfolio highlighting skills, I have, projects and resume',
+    githubRepo: 'https://github.com/jotex11/portfolio-challenge',
+    deployedSite: 'https://jotex11.github.io/portfolio-challenge/',
+    image: portfolio,
+    },
+    {
+        name: 'Javascript Quiz',
+        description: 'A timed quiz that changes keeps track of correct and wrong answers, and changes time if wrong or correct.',
+        githubRepo: 'https://github.com/jotex11/java-quiz',
+        deployedSite: 'https://jotex11.github.io/java-quiz/',
+        image: quiz,
+    },
+    {
+        name: 'Tunecast',
+        description: 'An app that reccomends music based on your current weather status from your location.',
+        githubRepo: 'https://github.com/jotex11/tunecast',
+        deployedSite: 'https://zelstart.github.io/tunecast/',
+        image: tunecast,
+    },
+    {
+        name: 'Milbook',
+        description: 'A blog that lets you post about a meme that is posted, and you can choose between a dog or cat as your profile status',
+        githubRepo: 'https://github.com/jotex11/disgruntled-millennials-group2',
+        deployedSite: 'https://young-citadel-13202-aee4f5f0145b.herokuapp.com/signup',
+        image: milbook,
+    },
+];
