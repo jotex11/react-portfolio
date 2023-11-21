a44ca03ec874a024a5f6512947174cd61e61b6f2
joseph <jomill0723@outlook.com>
2023-11-20 19:09:16 -0500

    fixed some small errors

---
+++ react-portfolio/src/components/nav/nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './nav.css';
+import '../../style.css';
+
+function Nav() {
+    return (
+        <nav className="container-fluid d-flex flex-column justify-content-center align-items-center">
+                <div className='navstuff'>
+                    <a className='navlink' href="/">about me</a>
+                    <a className='navlink' href="/portfolio">portfolio</a>
+                    <a className='navlink' href="/resume">resume</a>
+                    <a className='navlink' href="/contactme">contact me</a>
+                </div>
+        </nav>
+    )
+
+
+}
+
+export default Nav;
